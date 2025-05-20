463c670f006b1e0e239f2803a88b6f14914a71b8
DusanBilki <dusanbilkan63@gmail.com>
2025-05-20 03:05:29 +0200

    assets, img, hard-code data
    Created home component, movie model, movie service, user service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -13,7 +13,8 @@ import {MatIconModule} from '@angular/material/icon';
     RouterOutlet,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
